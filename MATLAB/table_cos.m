17549f15bbbd4c55bcd8fba0e134d415a68b52b4
Evgeniy Kazakov <kazakov33murom@mail.ru>
2024-07-08 20:23:32 +0300

    Change 16 in table

--- MATLAB/table_cos.m
+++ MATLAB/table_cos.m
@@ -1,14 +1,15 @@
 clc; clear all; close all;
+pkg load communications;
 
 clk = 250 * 1e6;
 t = 0:1/clk:5*1e-6-1/clk;
 f_dds_0 = 100*1e3;
-cosine_input = uint8(2^7/2*0.9*cos(2 * pi * f_dds_0 * t) + 2^7/2);
+cosine_input = uint16(2^16/2*0.9*cos(2 * pi * f_dds_0 * t) + 2^16/2);
 
 %figure()
 %plot(t, cosine_input)
 
-bit_cosine_input = de2bi(cosine_input, 7);
+bit_cosine_input = de2bi(cosine_input, 16)(:,10:16);
 filename = 'cos-table.txt';
 file = fopen(filename, 'w');
 for i = 1:size(bit_cosine_input,1)
