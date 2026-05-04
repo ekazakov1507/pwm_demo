0ffdcbeee154e8ff49e38eb06e902d4bf0b601f5
Evgeniy Kazakov <evgeniy_kazakov_93@mail.ru>
2026-05-04 22:05:40 +0300

    Use pwm_core submodule for PWM source manifest

--- tcl/pwm_core_sources.tcl
+++ tcl/pwm_core_sources.tcl
@@ -1,21 +1,14 @@
 set script_dir [file dirname [info script]]
 set repo_root [file dirname $script_dir]
 
-set pwm_core_vhdl_files [list \
-  [file join $repo_root src utils range_divider_pkg.vhd] \
+source [file join $repo_root pwm_core tcl pwm_core_sources.tcl]
+
+set pwm_demo_local_vhdl_files [list \
   [file join $repo_root src utils edge_delay.vhd] \
-  [file join $repo_root src utils dead_time_generator.vhd] \
-  [file join $repo_root src counters up_counter_unsigned.vhd] \
-  [file join $repo_root src counters up_counter_signed.vhd] \
-  [file join $repo_root src counters updown_counter_unsigned.vhd] \
-  [file join $repo_root src counters updown_counter_signed.vhd] \
   [file join $repo_root src buffers async_fifo.vhd] \
   [file join $repo_root src signal_chain data_decimator.vhd] \
-  [file join $repo_root src signal_chain scaler_unsigned.vhd] \
-  [file join $repo_root src signal_chain scaler_signed.vhd] \
   [file join $repo_root src signal_chain sine_gen_simple.vhd] \
-  [file join $repo_root src pwm pwm_1ch_drive_pkg.vhd] \
-  [file join $repo_root src pwm pwm_1ch.vhd] \
-  [file join $repo_root src pwm pwm_mch.vhd] \
   [file join $repo_root src pwm pwm_mch_buf.vhd] \
 ]
+
+set pwm_core_vhdl_files [concat $pwm_core_vhdl_files $pwm_demo_local_vhdl_files]
